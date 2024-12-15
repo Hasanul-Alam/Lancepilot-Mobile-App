d297e5e371e6a3f5d93b633f35144b72515545c0
Hasan <superdev0000@gmail.com>
2024-12-15 13:05:42 +0600

    Created a side menu bar

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,41 +1,39 @@
 import { Tabs } from "expo-router";
 import React, { useState } from "react";
 import {
-  Platform,
   Animated,
   TouchableOpacity,
   Text,
   View,
-  Pressable,
+  Dimensions,
   StyleSheet,
+  Image,
 } from "react-native";
-import { HapticTab } from "@/components/HapticTab";
-import TabBarBackground from "@/components/ui/TabBarBackground";
-import { Colors } from "@/constants/Colors";
-import { useColorScheme } from "@/hooks/useColorScheme";
-import { AntDesign, Entypo, MaterialCommunityIcons } from "@expo/vector-icons";
+import { AntDesign, MaterialCommunityIcons, Entypo, Feather, EvilIcons, Ionicons } from "@expo/vector-icons";
+
+const { width, height } = Dimensions.get("window");
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
-  const [isModalVisible, setModalVisible] = useState(false);
-  const [fadeAnim] = useState(new Animated.Value(0)); // Animation value for fading in and out
+  const [isSidebarVisible, setSidebarVisible] = useState(false);
+  const slideAnim = useState(new Animated.Value(-width))[0];
 
-  const toggleModal = () => {
-    if (!isModalVisible) {
-      setModalVisible(true);
-      // Fade in both the modal background and the content
-      Animated.timing(fadeAnim, {
-        toValue: 1,
-        duration: 300, // Faster fade-in
-        useNativeDriver: true,
-      }).start();
+  // Sidebar Menu toggler
+  const toggleSidebar = () => {
+    if (isSidebarVisible) {
+      // Close Sidebar
+      Animated.timing(slideAnim, {
+        toValue: -width,
+        duration: 300,
+        useNativeDriver: false,
+      }).start(() => setSidebarVisible(false));
     } else {
-      // Fade out the modal background and content together
-      Animated.timing(fadeAnim, {
+      // Open Sidebar
+      setSidebarVisible(true);
+      Animated.timing(slideAnim, {
         toValue: 0,
-        duration: 300, // Faster fade-out
-        useNativeDriver: true,
-      }).start(() => setModalVisible(false));
+        duration: 300,
+        useNativeDriver: false,
+      }).start();
     }
   };
 
@@ -43,115 +41,162 @@ export default function TabLayout() {
     <>
       <Tabs
         screenOptions={{
-          tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
+          tabBarActiveTintColor: "#4F46E5",
+          tabBarInactiveTintColor: "#9CA3AF",
           headerShown: false,
-          tabBarBackground: TabBarBackground,
-          tabBarStyle: {
-            ...Platform.select({
-              ios: {
-                position: "absolute",
-              },
-            }),
-            ...styles.tabBar,
-          },
+          tabBarStyle: styles.tabBar,
         }}
       >
+        {/* Dashboard */}
         <Tabs.Screen
           name="index"
           options={{
             title: "",
             tabBarIcon: ({ focused }) => (
-              <View className="flex justify-center items-center mt-7 h-full">
+              <View className="flex justify-center items-center">
                 <AntDesign
-                  size={23}
-                  name="appstore-o"
-                  color={focused ? "#6366F1" : "#00BF63"}
-                />
-              </View>
-            ),
-          }}
-        />
-        <Tabs.Screen
-          name="explore"
-          options={{
-            title: "",
-            tabBarIcon: ({ focused }) => (
-              <View className="flex justify-center items-center mt-7 h-full">
-                <MaterialCommunityIcons
-                  name="comment-processing-outline"
                   size={26}
-                  color={focused ? "#6366F1" : "#00BF63"}
+                  name="appstore-o"
+                  color={focused ? "#4F46E5" : "#9CA3AF"}
                 />
               </View>
             ),
           }}
         />
-        <Tabs.Screen
-          name="profile"
-          options={{
-            tabBarLabel: "", // Removes the text under the "+" button
-            tabBarIcon: () => (
-              <View className="absolute bottom-5 justify-center items-center h-full">
-                <TouchableOpacity
-                activeOpacity={1}
-                  className="w-16 h-16 bg-green-500 rounded-full justify-center items-center shadow-md"
-                  onPress={toggleModal}
-                >
-                  <AntDesign name="plus" size={30} color="white" />
-                </TouchableOpacity>
-              </View>
-            ),
-          }}
-        />
+
+        {/* Inbox */}
         <Tabs.Screen
           name="inbox"
           options={{
             title: "",
             tabBarIcon: ({ focused }) => (
-              <View className="flex justify-center items-center mt-7 h-full">
-                <Entypo
-                  size={23}
-                  name="bell"
-                  color={focused ? "#6366F1" : "#00BF63"}
+              <View className="flex justify-center items-center">
+                <MaterialCommunityIcons
+                  name="comment-processing-outline"
+                  size={28}
+                  color={focused ? "#4F46E5" : "#9CA3AF"}
                 />
               </View>
             ),
           }}
         />
+
+        {/* Action Button */}
+        <Tabs.Screen
+          name="action"
+          options={{
+            tabBarLabel: "",
+            tabBarIcon: () => (
+              <View className="absolute -top-10">
+                <TouchableOpacity
+                  activeOpacity={0.7}
+                  className="w-16 h-16 bg-green-500 rounded-full justify-center items-center shadow-lg"
+                  onPress={() => alert("Action button pressed!")}
+                >
+                  <AntDesign name="plus" size={32} color="white" />
+                </TouchableOpacity>
+              </View>
+            ),
+          }}
+        />
+
+        {/* Burger Menu */}
         <Tabs.Screen
           name="burgerMenu"
           options={{
             title: "",
             tabBarIcon: ({ focused }) => (
-              <View className=" justify-center items-center mt-7 h-full">
-                <Entypo
-                  size={25}
-                  name="menu"
-                  color={focused ? "#6366F1" : "#00BF63"}
-                />
-              </View>
+              <Entypo
+                name="menu"
+                size={28}
+                color={focused ? "#4F46E5" : "#9CA3AF"}
+              />
             ),
+          }}
+          listeners={{
+            tabPress: (e) => {
+              e.preventDefault(); // Prevent tab navigation
+              toggleSidebar(); // Open/Close the sidebar
+            },
           }}
         />
       </Tabs>
 
-      {/* Popup Modal */}
-      {isModalVisible && (
-        <Animated.View style={[styles.modalOverlay, { opacity: fadeAnim }]}>
-          <Pressable
-            className="absolute top-0 left-0 right-0 bottom-0"
-            onPress={toggleModal}
-          />
-          <View className="w-72 p-5 bg-white rounded-xl items-center">
-            <Text className="text-lg text-gray-800 mb-5">
-              This is the popup window!
-            </Text>
-            <TouchableOpacity
-              className="bg-green-500 py-2 px-6 rounded-lg"
-              onPress={toggleModal}
-            >
-              <Text className="text-white font-bold">Close</Text>
-            </TouchableOpacity>
+      {/* Sidebar Menu */}
+      {isSidebarVisible && (
+        <Animated.View
+          style={[styles.sidebar, { transform: [{ translateX: slideAnim }] }]}
+          className="bg-white"
+        >
+          <View className="mt-10">
+            {/* Close Button */}
+            <View className="flex-row items-center mt-5 justify-between">
+              {/* Logo */}
+              <View className="w-[40px] h-[30px] rounded-full overflow-hidden">
+                <Image
+                  source={require("../../assets/images/logo.png")}
+                  resizeMode="cover"
+                  className="w-full h-full"
+                />
+              </View>
+              <View className="w-[25px] h-[25px] bg-red-500 rounded-full flex-row items-center justify-center">
+                <TouchableOpacity
+                  onPress={toggleSidebar} // Close the sidebar
+                  className=" w-full"
+                >
+                  <Text className="text-white font-bold w-full text-center">
+                    X
+                  </Text>
+                </TouchableOpacity>
+              </View>
+            </View>
+            <View className="mt-7">
+                
+                <TouchableOpacity
+                  className=" mt-4 rounded-lg flex-row items-center py-2 px-3 gap-3"
+                  onPress={() => alert("Menu Item 1 clicked")}
+                >
+                  <View><MaterialCommunityIcons name="view-dashboard-outline" size={20}/></View>
+                  <Text className="text-black text-xl">
+                    Dashboard
+                  </Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                  className=" mt-4 rounded-lg  flex-row items-center py-2 px-3 gap-3"
+                  onPress={() => alert("Menu Item 1 clicked")}
+                >
+                  <View className="-mt-[4px]"><EvilIcons name="envelope" size={25}/></View>
+                  <Text className="text-black text-xl">
+                    Inbox
+                  </Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                  className=" mt-4 rounded-lg flex-row items-center py-2 px-3 gap-3"
+                  onPress={() => alert("Menu Item 1 clicked")}
+                >
+                  <View><Ionicons name="chatbubbles-outline" size={20}/></View>
+                  <Text className="text-black text-xl">
+                    Live Chat
+                  </Text>
+                </TouchableOpacity>
+
+              {/* <TouchableOpacity
+                className="bg-[#eff4ff] mt-4 rounded-full"
+                onPress={() => alert("Menu Item 2 clicked")}
+              >
+                <Text className="text-black text-base text-center py-2">
+                  Menu Item 2
+                </Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                className="bg-[#eff4ff] mt-4 rounded-full"
+                onPress={() => alert("Menu Item 3 clicked")}
+              >
+                <Text className="text-black text-base text-center py-2">
+                  Menu Item 3
+                </Text>
+              </TouchableOpacity> */}
+            </View>
           </View>
         </Animated.View>
       )}
@@ -161,28 +206,28 @@ export default function TabLayout() {
 
 const styles = StyleSheet.create({
   tabBar: {
-    height: 70,
-    backgroundColor: "white",
-    borderTopLeftRadius: 0,
-    borderTopRightRadius: 0,
-    // Adjusted for a more spread shadow
+    height: 50,
+    backgroundColor: "#F9FAFB",
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 20,
     shadowColor: "#000",
-    shadowOpacity: 0.3,  // Slightly reduced opacity for a more spread-out shadow
-    shadowRadius: 20,    // Increased radius for a more spread-out shadow
-    shadowOffset: { width: 0, height: 10 },  // Adjusted offset for a wider shadow spread
-    elevation: 20,  // Increased elevation for Android
-    zIndex: 0,     // Ensures tab bar is on top and visible
-    borderTopWidth: 2,
+    shadowOpacity: 0.2,
+    shadowRadius: 10,
+    shadowOffset: { width: 0, height: 5 },
+    elevation: 10,
   },
-  modalOverlay: {
-    flex: 1,
-    backgroundColor: "rgba(0, 0, 0, 0.5)", // Semi-transparent overlay
-    justifyContent: "center",
-    alignItems: "center",
+  sidebar: {
     position: "absolute",
     top: 0,
     left: 0,
-    right: 0,
-    bottom: 0,
+    height: "100%",
+    width: width * 0.7, // Sidebar width should be 75% of the screen
+    // backgroundColor: "#fff", // Purple background
+    padding: 20,
+    zIndex: 10, // Ensure it stays above other content
+    shadowColor: "#000",
+    shadowOpacity: 0.3,
+    shadowRadius: 15,
+    elevation: 5,
   },
 });
--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -1,133 +1,133 @@
-import {
-  StyleSheet,
-  Image,
-  View,
-  Text,
-  TextInput,
-  TouchableOpacity,
-} from "react-native";
-import { useState } from "react";
-import { Feather } from "@expo/vector-icons";
+// import {
+//   StyleSheet,
+//   Image,
+//   View,
+//   Text,
+//   TextInput,
+//   TouchableOpacity,
+// } from "react-native";
+// import { useState } from "react";
+// import { Feather } from "@expo/vector-icons";
 
-export default function TabTwoScreen() {
-  const [password, setPassword] = useState("");
-  const [showPassword, setShowPassword] = useState(false);
-  return (
-    <View className="flex-1 justify-center items-center w-[90%] mx-auto">
-      {/* Lancepilot logo */}
-      <View style={styles.imageContainer} className="">
-        <Image
-          source={require("../../assets/images/logo.png")}
-          style={styles.image}
-          resizeMode="contain"
-        />
-      </View>
-      <Text className="text-3xl mb-5">Please Login</Text>
-      {/* Email Input Field */}
-      <TextInput
-        className="border border-1 border-[#9494b8] w-full py-3 px-3 rounded"
-        placeholder="email@lancepilot.com"
-        keyboardType="email-address"
-        autoCapitalize="none"
-      />
+// export default function TabTwoScreen() {
+//   const [password, setPassword] = useState("");
+//   const [showPassword, setShowPassword] = useState(false);
+//   return (
+//     <View className="flex-1 justify-center items-center w-[90%] mx-auto">
+//       {/* Lancepilot logo */}
+//       <View style={styles.imageContainer} className="">
+//         <Image
+//           source={require("../../assets/images/logo.png")}
+//           style={styles.image}
+//           resizeMode="contain"
+//         />
+//       </View>
+//       <Text className="text-3xl mb-5">Please Login</Text>
+//       {/* Email Input Field */}
+//       <TextInput
+//         className="border border-1 border-[#9494b8] w-full py-3 px-3 rounded"
+//         placeholder="email@lancepilot.com"
+//         keyboardType="email-address"
+//         autoCapitalize="none"
+//       />
 
-      <View className="border border-1 border-[#9494b8] w-full my-3 px-3 rounded flex-row items-center justify-between">
-        {/* Password Input Field */}
-        <TextInput
-          className="flex-1 h-12 text-base"
-          placeholder="***********"
-          placeholderTextColor="#b0b0d4"
-          secureTextEntry={!showPassword}
-          autoCapitalize="none"
-        />
-        {/* Show & Hide Password Button */}
-        <TouchableOpacity
-          onPress={() => setShowPassword(!showPassword)}
-          activeOpacity={0.7}
-        >
-          <Text className="text-blue-500 ml-3">
-            {showPassword ? (
-              <Feather
-                name="eye-off"
-                size={18}
-                color="gray"
-                className=""
-              />
-            ) : (
-              <Feather
-                name="eye"
-                size={18}
-                color="gray"
-                className=""
-              />
-            )}
-          </Text>
-        </TouchableOpacity>
-      </View>
-      {/* Login Button */}
-      <TouchableOpacity
-        className="w-full bg-[#00BF63] py-2 mt-2 rounded-lg"
-        activeOpacity={0.8}
-      >
-        <Text className="text-center font-semibold text-lg text-white">
-          LOGIN
-        </Text>
-      </TouchableOpacity>
+//       <View className="border border-1 border-[#9494b8] w-full my-3 px-3 rounded flex-row items-center justify-between">
+//         {/* Password Input Field */}
+//         <TextInput
+//           className="flex-1 h-12 text-base"
+//           placeholder="***********"
+//           placeholderTextColor="#b0b0d4"
+//           secureTextEntry={!showPassword}
+//           autoCapitalize="none"
+//         />
+//         {/* Show & Hide Password Button */}
+//         <TouchableOpacity
+//           onPress={() => setShowPassword(!showPassword)}
+//           activeOpacity={0.7}
+//         >
+//           <Text className="text-blue-500 ml-3">
+//             {showPassword ? (
+//               <Feather
+//                 name="eye-off"
+//                 size={18}
+//                 color="gray"
+//                 className=""
+//               />
+//             ) : (
+//               <Feather
+//                 name="eye"
+//                 size={18}
+//                 color="gray"
+//                 className=""
+//               />
+//             )}
+//           </Text>
+//         </TouchableOpacity>
+//       </View>
+//       {/* Login Button */}
+//       <TouchableOpacity
+//         className="w-full bg-[#00BF63] py-2 mt-2 rounded-lg"
+//         activeOpacity={0.8}
+//       >
+//         <Text className="text-center font-semibold text-lg text-white">
+//           LOGIN
+//         </Text>
+//       </TouchableOpacity>
 
-      {/* Devider */}
-      <View className="justify-center items-center">
-        <View className="flex-row items-center my-5">
-          <View className="w-full h-[1px] bg-black opacity-40 flex-1"></View>
-          <Text className="mx-1 mb-1 text-lg">or</Text>
-          <View className="w-full h-[1px] bg-black opacity-40 flex-1"></View>
-        </View>
-      </View>
+//       {/* Devider */}
+//       <View className="justify-center items-center">
+//         <View className="flex-row items-center my-5">
+//           <View className="w-full h-[1px] bg-black opacity-40 flex-1"></View>
+//           <Text className="mx-1 mb-1 text-lg">or</Text>
+//           <View className="w-full h-[1px] bg-black opacity-40 flex-1"></View>
+//         </View>
+//       </View>
 
-      {/* Google Sign-In */}
-      <View className="bg-gray-300 w-full rounded-lg">
-        <TouchableOpacity
-          className="flex-row items-center justify-center py-2"
-          activeOpacity={0.5}
-        >
-          <View className="w-[20px] h-[20px]">
-            <Image
-              source={require("../../assets/images/google-icon.png")}
-              resizeMode="contain"
-              className="w-full h-full"
-            />
-          </View>
-          <Text className="text-black mx-3 text-lg">Continue With Google</Text>
-        </TouchableOpacity>
-      </View>
+//       {/* Google Sign-In */}
+//       <View className="bg-gray-300 w-full rounded-lg">
+//         <TouchableOpacity
+//           className="flex-row items-center justify-center py-2"
+//           activeOpacity={0.5}
+//         >
+//           <View className="w-[20px] h-[20px]">
+//             <Image
+//               source={require("../../assets/images/google-icon.png")}
+//               resizeMode="contain"
+//               className="w-full h-full"
+//             />
+//           </View>
+//           <Text className="text-black mx-3 text-lg">Continue With Google</Text>
+//         </TouchableOpacity>
+//       </View>
 
-      {/* Terms & Privacy Plicy */}
-      <View className="mt-7">
-        <Text className="text-gray-500 text-sm text-center">
-          By Clicking Continue, you agree to our{" "}
-          <Text className="text-black">Terms of Service</Text> and{" "}
-          <Text className="text-black">Privacy Policy</Text>
-        </Text>
-      </View>
-    </View>
-  );
-}
+//       {/* Terms & Privacy Plicy */}
+//       <View className="mt-7">
+//         <Text className="text-gray-500 text-sm text-center">
+//           By Clicking Continue, you agree to our{" "}
+//           <Text className="text-black">Terms of Service</Text> and{" "}
+//           <Text className="text-black">Privacy Policy</Text>
+//         </Text>
+//       </View>
+//     </View>
+//   );
+// }
 
-const styles = StyleSheet.create({
-  mainContainer: {
-    flex: 1,
-  },
-  redContainer: {
-    flex: 1,
-  },
-  blueContainer: {
-    flex: 3,
-  },
-  imageContainer: {
-    width: 50,
-    height: 50,
-  },
-  image: {
-    width: "100%",
-    height: "100%",
-  },
-});
+// const styles = StyleSheet.create({
+//   mainContainer: {
+//     flex: 1,
+//   },
+//   redContainer: {
+//     flex: 1,
+//   },
+//   blueContainer: {
+//     flex: 3,
+//   },
+//   imageContainer: {
+//     width: 50,
+//     height: 50,
+//   },
+//   image: {
+//     width: "100%",
+//     height: "100%",
+//   },
+// });
